ed7eccdf84a4decb8f8e1a4fa6544480363daf6e
Ankit <ankit0839483@gmail.com>
2024-11-01 22:28:45 +0530

    minor changes

--- cricket.js
+++ cricket.js
@@ -2,7 +2,9 @@
 let pone=0;
 let ptwo=0;
 let game_value=0;
-function play_game(){
+
+function play_game()
+{
     game_value=Math.floor((Math.random()+0)*7);
     
     document.getElementById("score_img").style.height="400px";
@@ -30,61 +32,69 @@ function play_game(){
         document.getElementById("score_img").src="assets/six.gif";
     }
 }
-function next(){
+
+function next()
+{
     document.getElementById("entry").style.display="none";
     document.getElementById("gp").style.display="block";
 }
-function ccc(){
+function ccc()
+{
     document.getElementById("hide-img").style.display="none";
     document.getElementById("entry").style.display="block";
 }
-$(document).ready(function(){
-            var x = document.getElementById("audio");
-            $(".game_logo").click(function(){
-                x.play();
-            });
-        
-            $("#play_one").click(function(){
-                pone=pone+1;
-                if(pone>=7){
-                    alert("YOUR SCORE IS");
-                    $("#p_one").html(eval($("#p_one").html()));
-                    $("#p_two").html(eval($("#p_two").html()));
-                }
-                else {
-                    play_game();
-                    if(pone==1){
-                        $("#p_one").html($("#p_one").html()+game_value);
-                    }
-                    else{
-                        $("#p_one").html($("#p_one").html()+"+"+game_value);
-                    }
-                    $("#play_one").attr("disabled","disabled");
-                    $("#play_two").removeAttr("disabled");
-                }
-                
-            });
-            $("#play_two").click(function(){
-                ptwo=ptwo+1;
-                if(ptwo>=7){
-                    alert("Click here to see the result");
-                    $("#p_one").html(eval($("#p_one").html()));
-                    $("#p_two").html(eval($("#p_two").html()));
-                }
-                else {
-                    play_game(); 
-                    if(ptwo==1){
-                        $("#p_two").html($("#p_two").html()+game_value);
-                    }
-                    else{
-                        $("#p_two").html($("#p_two").html()+"+"+game_value);
-                    }
-                    $("#play_two").attr("disabled","disabled");
-                    $("#play_one").removeAttr("disabled");
-                }
-            });
-        });
-        $("#strt-btn").click(function(){
+$(document).ready(function()
+{
+    var x = document.getElementById("audio");
+    $(".game_logo").click(function(){
+        x.play();
+    });
+
+$("#play_one").click(function(){
+    pone=pone+1;
+    if(pone>=7)
+    {
+        alert("YOUR SCORE IS");
+        $("#p_one").html(eval($("#p_one").html()));
+        $("#p_two").html(eval($("#p_two").html()));
+    }
+    else
+    {
+        play_game();
+        if(pone==1){
+            $("#p_one").html($("#p_one").html()+game_value);
+        }
+        else{
+            $("#p_one").html($("#p_one").html()+"+"+game_value);
+        }
+        $("#play_one").attr("disabled","disabled");
+        $("#play_two").removeAttr("disabled");
+    }
+    });
+    $("#play_two").click(function(){
+    ptwo=ptwo+1;
+    if(ptwo>=7)
+    {
+        alert("Click here to see the result");
+        $("#p_one").html(eval($("#p_one").html()));
+        $("#p_two").html(eval($("#p_two").html()));
+    }
+    else
+    {
+        play_game(); 
+        if(ptwo==1){
+            $("#p_two").html($("#p_two").html()+game_value);
+        }
+        else{
+            $("#p_two").html($("#p_two").html()+"+"+game_value);
+        }
+        $("#play_two").attr("disabled","disabled");
+        $("#play_one").removeAttr("disabled");
+    }
+    });
+});
+        $("#strt-btn").click(function()
+        {
             let ap=document.getElementById("player_1_name").value;
             if(ap==="")
             {
@@ -94,19 +104,20 @@ $(document).ready(function(){
             }
             else{}
         });
-        $("#strt-btn").click(function(){
+        $("#strt-btn").click(function()
+        {
             let ap2=document.getElementById("player_2_name").value;
             if(ap2==="")
             {
                 confirm("PLEASE ENTER YOUR NAME");
                 window.location.href = "file:///C:/Users/ankit/Desktop/PROJECTS/cricket.html";
             }
-            else{
-                
+            else{   
             }
         });
 
-$(document).ready(function(){
+$(document).ready(function()
+{
     var x = document.getElementById("audio");
     $(".game_logo").click(function(){
         x.play();
